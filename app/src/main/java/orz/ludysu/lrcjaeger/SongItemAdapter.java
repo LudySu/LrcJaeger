be1d4c421712b29234ae2aae1469213036547986
Ludy Su <ludysu@gmail.com>
2015-10-08 23:46:45 +1300

    Add a view holder in adapter to avoid findViewById multiple times

--- app/src/main/java/orz/ludysu/lrcjaeger/SongItemAdapter.java
+++ app/src/main/java/orz/ludysu/lrcjaeger/SongItemAdapter.java
@@ -1,9 +1,5 @@
 package orz.ludysu.lrcjaeger;
 
-import java.util.ArrayList;
-
-import orz.ludysu.lrcjaeger.R;
-
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
@@ -12,6 +8,8 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SongItemAdapter extends ArrayAdapter<SongItem> {
     private LayoutInflater mInflater = null;
@@ -31,23 +29,37 @@ public class SongItemAdapter extends ArrayAdapter<SongItem> {
     public void setLrcClickListener(OnLrcClickListener l) {
         mLrcListener = l;
     }
-    
+
+    private class ViewHolder {
+        TextView mTitle;
+        TextView mArtist;
+        TextView mHasLrc;
+        int mPosition;
+    }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.song_item, parent, false);
+            holder = new ViewHolder();
+            holder.mTitle = (TextView) convertView.findViewById(R.id.tv_song_title);
+            holder.mArtist = (TextView) convertView.findViewById(R.id.tv_song_artist);
+            holder.mHasLrc = (TextView) convertView.findViewById(R.id.tv_has_lrc);
+            holder.mPosition = position;
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
-        
+
         SongItem song = mSongs.get(position);
-        
-        TextView title = (TextView) convertView.findViewById(R.id.tv_song_title);
-        title.setText(song.getTitle());
-        
-        TextView artist = (TextView) convertView.findViewById(R.id.tv_song_artist);
-        artist.setText(song.getArtist());
+
+        holder.mTitle.setText(song.getTitle());
+        holder.mArtist.setText(song.getArtist());
 
         int color = song.isHasLrc() ? Color.argb(255, 0, 162, 232) : Color.LTGRAY;
-        TextView hasLrc = (TextView) convertView.findViewById(R.id.tv_has_lrc);
+        TextView hasLrc = holder.mHasLrc;
         hasLrc.setTextColor(color);
         
         hasLrc.setClickable(true);
